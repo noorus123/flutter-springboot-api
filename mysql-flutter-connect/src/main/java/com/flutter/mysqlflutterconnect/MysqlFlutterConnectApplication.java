a0146469fbc4d439a7be5853bbfc5756fd44e8f8
noorus123 <nooruskhan786@gmail.com>
2020-08-02 15:46:37 +0530

    initial raw skeleton

---
+++ mysql-flutter-connect/src/main/java/com/flutter/mysqlflutterconnect/MysqlFlutterConnectApplication.java
@@ -0,0 +1,13 @@
+package com.flutter.mysqlflutterconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlFlutterConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlFlutterConnectApplication.class, args);
+	}
+
+}
---
+++ mysql-flutter-connect/src/test/java/com/flutter/mysqlflutterconnect/MysqlFlutterConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flutter.mysqlflutterconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlFlutterConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
